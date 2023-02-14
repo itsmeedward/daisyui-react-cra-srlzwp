7a300f0931cb1697e1895a6b03a435173808c050
itsmeedward <86776978+itsmeedward@users.noreply.github.com>
2023-02-14 04:04:59 +0100

    1 binaries uploaded

--- src/app.js
+++ src/app.js
@@ -2,20 +2,17 @@ import React from 'react';
 import Header from './components/header';
 import Footer from './components/footer';
 import Main from './pages/main';
-import {
-  BrowseRouter as Router,
-  BrowserRouter,
-  link,
-  Route,
-} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Faq from './pages/faq';
 
 const App = (props) => {
   return (
     <div>
       <Header />
-      <BrowserRouter>
-        <Route path="Main" />
-      </BrowserRouter>
+
+      <Main/>
+        
+
       <Footer />
     </div>
   );
